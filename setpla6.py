b797a9d65c5c1230dc6f39d063fddc3276d7ecd3
soniaaji <45995179+soniaaji@users.noreply.github.com>
2019-01-06 06:46:53 -0800

    Create setpla6.py

---
+++ setpla6.py
@@ -0,0 +1,7 @@
+Input:  str1 = "AAB", str2 = "XXY"
+Output: True
+'A' is mapped to 'X' and 'B' is mapped to 'Y'
+Input:  str1 = "AAB", str2 = "XYZ"
+Output: False
+One occurrence of 'A' in str1 has 'X' in str2 and 
+other occurrence of 'A' has 'Y'
